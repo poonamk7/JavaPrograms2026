b3c1c9e198520c862e58aab502b1625c7a67c042
Poonam <kadampoonam7194@gmail.com>
2026-01-03 11:21:54 +0530

    Modified Majority element code

--- src/test/java/basicprograms/MajorityElement.java
+++ src/test/java/basicprograms/MajorityElement.java
@@ -1,5 +1,8 @@
 package basicprograms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement {
 
 	// This Java program is to find Majority element from a given array.
@@ -7,9 +10,11 @@ public class MajorityElement {
 	
 	public static void main(String[] args) {
 		
-		int[] arr = {1,3,5,1,1,7,7,1,1}; //here 1 is appeared more than n/2 [n=length of an array]
+		int[] arr = {7,2,1,7,7,7,8,7,7,2}; //here 1 is appeared more than n/2 [n=length of an array]
 		
-		System.out.println("Majority element is: "+getMajorityelement(arr));
+//		System.out.println("Majority element is: "+getMajorityelement(arr));
+		
+		System.out.println("Majority element is: "+getMajorityelementUsingHashMap(arr));
 	}
 	
 	
@@ -35,5 +40,27 @@ public class MajorityElement {
 		return candidate;
 		
 	}
-
+	
+	
+	/* Description: Helper function to get Majority element using HashMap
+	   Created by: Poonam Kadam
+	   Created Date: 03/01/2026
+	   Modified Date:
+	   Modified By: 
+	*/
+	public static int getMajorityelementUsingHashMap(int[] arr) {
+		int candidate=0;
+		Map<Integer, Integer> majorityMap = new HashMap<Integer, Integer>();
+		
+		for(int num: arr) {
+			majorityMap.put(num, majorityMap.getOrDefault(num, 0)+1);
+		}
+		
+		for(Map.Entry<Integer, Integer> entry:majorityMap.entrySet()) {
+			if(entry.getValue()>(arr.length/2)) {
+				candidate=entry.getKey();
+			}
+		}
+		return candidate;
+	}
 }
