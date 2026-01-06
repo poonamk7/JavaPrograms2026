c60031404385027f3b11947dd26037e67ef99275
Poonam <kadampoonam7194@gmail.com>
2026-01-06 22:05:48 +0530

    added another logic using Java 8

--- src/test/java/arrayprograms/ShiftNegativesToLeftAndZeroInMiddle.java
+++ src/test/java/arrayprograms/ShiftNegativesToLeftAndZeroInMiddle.java
@@ -1,6 +1,7 @@
 package arrayprograms;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ShiftNegativesToLeftAndZeroInMiddle {
 	
@@ -9,7 +10,9 @@ public class ShiftNegativesToLeftAndZeroInMiddle {
 	public static void main(String[] args) {
 		
 		int[] arr = {0,3,-2,0,-3,4,9,7,0,-1,5};
-		System.out.println("Output: "+Arrays.toString(shiftElements(arr) ));
+//		System.out.println("Output: "+Arrays.toString(shiftElements(arr) ));
+		
+		System.out.println("Result: "+Arrays.toString(sortingUsingJava8(arr)));
 	}
 	
 	
@@ -45,6 +48,23 @@ public class ShiftNegativesToLeftAndZeroInMiddle {
 		
 		return result;
 		
+	}
+	
+	/*
+	 * Description: Helper function to arrange elements with nonzero numbers in the beginning followed by zeroes followed by negative numbers using Java 8
+	 * Created by: Poonam Kadam 
+	 * Created Date: 06/01/2026 
+	 * Modified Date:
+	 * Modified By:
+	 */
+	public static int[] sortingUsingJava8(int[] arr) {
+		
+		int[] result = IntStream.concat
+				(IntStream.concat(Arrays.stream(arr).filter(x -> x>0), Arrays.stream(arr)
+						.filter(x -> x==0)), Arrays.stream(arr).filter(x -> x<0)
+						).toArray();
+		
+		return result;
 	}
 
 }
